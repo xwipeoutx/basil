dd0e8fa8d04930716b354c46e4786fdfc303d325
xwipeoutx <xwipeoutx@gmail.com>
2012-11-29 23:32:37 +1000

    All QUnit tests are now included, and pass.  Well, the ones I care about anyway ...

--- adapters/qunit/chai-qunit.js
+++ adapters/qunit/chai-qunit.js
@@ -1,20 +1,33 @@
 (function(global) {
-
     QUnit.assert = {};
     override('ok', chai.assert.ok);
     override('equal', chai.assert.equal);
     override('deepEqual', chai.assert.deepEqual);
     override('strictEqual', chai.assert.strictEqual);
     override('notStrictEqual', chai.assert.notStrictEqual);
+    override('throws',qunitThrows );
+    override('raises',qunitThrows );
+    override('push', function(expression, actual, expected, message) {
+        chai.assert(expression, message);
+    });
+    override('propEqual', function() {
+        chai.assert(false, '"propEqual"is  not supported for Chai-Basil bridge');
+    });
+
+    function qunitThrows(fn, expected, message) {
+        if (typeof expected == 'function')
+            return;
+
+        fn = fn.bind(QUnit.current_testEnvironment);
+        chai.assert.throws.call(this, fn, expected, message);
+    }
 
     function override(name, chaiEquivalent) {
         var qunitAssertFn = function() {
-            QUnit.current.numAsserts++;
+            QUnit.current_testEnvironment.numAsserts++;
             chaiEquivalent.apply(this, arguments);
         };
 
         QUnit.assert[name] = QUnit[name] = global[name] = qunitAssertFn;
-
     }
-
 })(this);
--- adapters/qunit/qunit-adapter.js
+++ adapters/qunit/qunit-adapter.js
@@ -1,40 +1,69 @@
 (function(global) {
-    var numAssertionsToExpect, currentModule={};
+    var numAssertionsToExpect, currentModule = {}, fixture;
     global.expect = function(val) {return val == null ? numAssertionsToExpect : (numAssertionsToExpect = val)};
     global.module = function(name, args) {
         currentModule = args || {};
     };
+
+    // Not supported
     global.start = function() {};
     global.stop = function() {};
+    global.asyncTest = function() {};
 
     global.test = function(name, expects, fn) {
+
+        if (typeof expects == "function") {
+            fn = expects;
+            expects = -1;
+        }
+
         var module = currentModule;
         describe(name, function() {
-            if (typeof expects == "function") {
-                fn = expects;
-                expects = -1;
+            resetQUnitFixture();
+
+            QUnit.current_testEnvironment = {};
+            for (var key in module) {
+                QUnit.current_testEnvironment[key] = module[key];
             }
+            QUnit.current_testEnvironment.numAsserts = 0;
 
-            QUnit.current = {
-                numAsserts: 0
-            };
+            if (module.setup) module.setup.call(QUnit.current_testEnvironment);
 
-            if (module.setup) module.setup();
+            expect(expects);
 
-            //it(name, function() {
-                expect(expects);
-                fn(QUnit.assert);
-                if (expects > -1)
-                equal(expect(), QUnit.current.numAsserts);
-            //});
+            var oldSetTimeout = global.setTimeout;
+            global.setTimeout = function(afterTimeout, i) { afterTimeout(); };
+            fn.call(QUnit.current_testEnvironment, QUnit.assert);
+            global.setTimeout = oldSetTimeout;
 
-            if (module.teardown) module.teardown();
+            if (expects > -1)
+                equal(expect(), QUnit.current_testEnvironment.numAsserts);
+
+            if (module.teardown) module.teardown.call(QUnit.current_testEnvironment);
         });
     };
 
     global.QUnit = {
         expect: global.expect,
         module: global.module,
-        test: global.test
+        test: global.test,
+        equiv: function() {
+            QUnit.push(false, false, false, '"QUnit.equiv" not supported for Basil\'s QUnit Adapter');
+        },
+        done: function() {},
+        jsDump: {
+            parse: function() { QUnit.push(false, false, false, '"QUnit.jsDump" not supported for Basil\'s QUnit Adapter');}
+        }
     };
+
+    var previousValue = null;
+
+    function resetQUnitFixture () {
+        if (!document.body)
+            return;
+
+        fixture = document.getElementById('qunit-fixture');
+        previousValue = previousValue || fixture.innerHTML;
+        fixture.innerHTML = previousValue;
+    }
 })(this);
