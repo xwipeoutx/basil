ca59a11ec4cb7ca1690f89d7ecb8721b2454cac5
xwipeoutx <xwipeoutx@gmail.com>
2012-12-13 00:34:31 +1000

    Updated sample tests

--- samples/sample-tests.js
+++ samples/sample-tests.js
@@ -9,18 +9,14 @@ describe("Teapot", function() {
         when("adding water", function() {
             teapot.addWater();
 
-            it("has water", function() {
-                expect(teapot.isEmpty()).to.be.false;
-            });
+            it(function() { expect(teapot.isEmpty()).to.be.false; });
         });
     });
 
     when("has water", function() {
         teapot.addWater();
 
-        it("is not empty", function() {
-            expect(teapot.isEmpty()).to.be.false;
-        });
+        it(function() { expect(teapot.isEmpty()).to.be.false; });
 
         it("cannot have water added", function() {
             expect(function() {
@@ -31,9 +27,7 @@ describe("Teapot", function() {
         when("drained", function() {
             teapot.drain();
 
-            it("is empty", function() {
-                expect(teapot.isEmpty()).to.be.true;
-            });
+            it(function() { expect(teapot.isEmpty()).to.be.true; });
         });
     });
 });
