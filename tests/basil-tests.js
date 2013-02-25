f86af411d7c103ed3aabda6e90d390c90446c2df
xwipeoutx <xwipeoutx@gmail.com>
2013-02-25 23:08:36 +1000

    Added another layer in test runner tests

--- tests/basil-tests.js
+++ tests/basil-tests.js
@@ -71,196 +71,200 @@
     describe("TestRunner", function() {
         var sut = new Basil.TestRunner();
 
-        when("running empty test method", function() {
-            var testFunction = sinon.stub();
-            var result = sut.test("TestName", testFunction);
+        describe("test methods", function() {
+            when("running empty test method", function() {
+                var testFunction = sinon.stub();
+                var result = sut.test("TestName", testFunction);
 
-            then(function() { expect(result.isComplete()).to.be.true; });
-            then(function() { expect(result.runCount()).to.equal(1); });
-            then(function() { expect(testFunction).to.not.have.been.calledOn(undefined); });
+                then(function() { expect(result.isComplete()).to.be.true; });
+                then(function() { expect(result.runCount()).to.equal(1); });
+                then(function() { expect(testFunction).to.not.have.been.calledOn(undefined); });
 
-            when("running another empty test method", function() {
-                var testFunction2 = sinon.stub();
-                var result2 = sut.test("AnotherTestName", function() {});
-                then(function() { expect(result2.isComplete()).to.be.true; });
-                then(function() { expect(result2.runCount()).to.equal(1); });
-                then(function() { expect(testFunction2).to.not.have.been.calledOn(testFunction.firstCall.thisValue); });
-            });
-        });
-
-        when("running a test method with an inner test method call", function() {
-            var innerResult;
-            var innerTestFunction = sinon.stub();
-            var testFunction = function() {
-                innerResult = sut.test("Inner Test", innerTestFunction);
-            };
-            testFunction = this.spy(testFunction);
-            var result = sut.test("Outer Test", testFunction);
-
-            then(function() { expect(result.isComplete()).to.be.true; });
-            then(function() { expect(result.runCount()).to.equal(1); });
-            then(function() { expect(result.children()).to.deep.equal([innerResult]); });
-
-            then("inner function uses same 'this' value as outer", function() {
-                expect(innerTestFunction).to.have.been.calledOn(testFunction.firstCall.thisValue);
-            });
-        });
-
-        when("running a test method with 2 inner test method calls", function() {
-            var innerResult, innerResult2;
-            var innerFunction1 = sinon.stub();
-            var innerFunction2 = sinon.stub();
-            var testFunction = function() {
-                innerResult = sut.test("Inner Test", innerFunction1);
-                innerResult2 = sut.test("Inner Test 2", innerFunction2);
-            };
-            testFunction = this.spy(testFunction);
-            var result = sut.test("Outer Test", testFunction);
-
-            then(function() { expect(result.isComplete()).to.be.true; });
-            then(function() { expect(result.runCount()).to.equal(2); });
-            then(function() { expect(result.children()).to.deep.equal([innerResult, innerResult2]); });
-
-            then("each outer call has different 'this' values", function() {
-                expect(testFunction.firstCall.thisValue).to.not.equal(testFunction.secondCall.thisValue);
-            });
-
-            then("inner function 1 uses same 'this' value as first outer call", function() {
-                expect(innerFunction1).to.have.been.calledOn(testFunction.firstCall.thisValue);
-            });
-
-            then("inner function 2 uses same 'this' value as second outer call", function() {
-                expect(innerFunction2).to.have.been.calledOn(testFunction.secondCall.thisValue);
-            });
-        });
-
-        when("running a test method with 3 inner test method calls", function() {
-            var innerResult, innerResult2, innerResult3;
-            var result = sut.test("Outer Test", function() {
-                innerResult = sut.test("Inner Test", function() {});
-                innerResult2 = sut.test("Inner Test 2", function() {});
-                innerResult3 = sut.test("Inner Test 3", function() {});
-            });
-
-            then(function() { expect(result.isComplete()).to.be.true; });
-            then(function() { expect(result.runCount()).to.equal(3); });
-            then(function() { expect(result.children()).to.deep.equal([innerResult, innerResult2, innerResult3]); });
-        });
-
-        when("running a test method with a double nested test method call", function() {
-            var innerResult, innerInnerResult, innerInnerResult2;
-            var result = sut.test("Outer Test", function() {
-                innerResult = sut.test("Inner Test", function() {
-                    innerInnerResult = sut.test("Inner Inner Test", function() {});
-                    innerInnerResult2 = sut.test("Inner Inner Test 2", function() {});
+                when("running another empty test method", function() {
+                    var testFunction2 = sinon.stub();
+                    var result2 = sut.test("AnotherTestName", function() {});
+                    then(function() { expect(result2.isComplete()).to.be.true; });
+                    then(function() { expect(result2.runCount()).to.equal(1); });
+                    then(function() { expect(testFunction2).to.not.have.been.calledOn(testFunction.firstCall.thisValue); });
                 });
             });
 
-            then(function() { expect(result.isComplete()).to.be.true; });
-            then(function() { expect(result.runCount()).to.equal(2); });
-            then(function() { expect(result.children()).to.deep.equal([innerResult]); });
-            then(function() { expect(innerResult.children()).to.deep.equal([innerInnerResult, innerInnerResult2]); });
-        });
+            when("running a test method with an inner test method call", function() {
+                var innerResult;
+                var innerTestFunction = sinon.stub();
+                var testFunction = function() {
+                    innerResult = sut.test("Inner Test", innerTestFunction);
+                };
+                testFunction = this.spy(testFunction);
+                var result = sut.test("Outer Test", testFunction);
 
-        when("running a named test method without specifying a name", function() {
-            var result = sut.test(function NamedFunctionName () {});
-            then(function() { expect(result.name()).to.equal("NamedFunctionName"); });
-        });
+                then(function() { expect(result.isComplete()).to.be.true; });
+                then(function() { expect(result.runCount()).to.equal(1); });
+                then(function() { expect(result.children()).to.deep.equal([innerResult]); });
 
-        when("running an unnamed test method without specifying a name", function() {
-            var result = sut.test(function() { expect('name').to.equal('name'); });
-            then(function() { expect(result.name()).to.equal("expect name to equal name"); });
-        });
-
-        when("running a named test method without specifying a name", function() {
-            var result = sut.test(function TestName () {});
-
-            then("name extracted from test name", function() {
-                expect(result.name()).to.equal("TestName");
-            });
-        });
-
-        when("registering a root plugin", function() {
-            var pluginFunction = sinon.stub();
-            sut.registerRootPlugin(pluginFunction);
-
-            then(function() { expect(pluginFunction).to.not.have.been.called; });
-
-            when("plugin does not yield", function() {
-                it("throws", function() {
-                    expect(function() { sut.test("TestName", function() {}); }).to.throw();
+                then("inner function uses same 'this' value as outer", function() {
+                    expect(innerTestFunction).to.have.been.calledOn(testFunction.firstCall.thisValue);
                 });
             });
 
-            when("plugin yields", function() {
+            when("running a test method with 2 inner test method calls", function() {
+                var innerResult, innerResult2;
+                var innerFunction1 = sinon.stub();
+                var innerFunction2 = sinon.stub();
+                var testFunction = function() {
+                    innerResult = sut.test("Inner Test", innerFunction1);
+                    innerResult2 = sut.test("Inner Test 2", innerFunction2);
+                };
+                testFunction = this.spy(testFunction);
+                var result = sut.test("Outer Test", testFunction);
+
+                then(function() { expect(result.isComplete()).to.be.true; });
+                then(function() { expect(result.runCount()).to.equal(2); });
+                then(function() { expect(result.children()).to.deep.equal([innerResult, innerResult2]); });
+
+                then("each outer call has different 'this' values", function() {
+                    expect(testFunction.firstCall.thisValue).to.not.equal(testFunction.secondCall.thisValue);
+                });
+
+                then("inner function 1 uses same 'this' value as first outer call", function() {
+                    expect(innerFunction1).to.have.been.calledOn(testFunction.firstCall.thisValue);
+                });
+
+                then("inner function 2 uses same 'this' value as second outer call", function() {
+                    expect(innerFunction2).to.have.been.calledOn(testFunction.secondCall.thisValue);
+                });
+            });
+
+            when("running a test method with 3 inner test method calls", function() {
+                var innerResult, innerResult2, innerResult3;
+                var result = sut.test("Outer Test", function() {
+                    innerResult = sut.test("Inner Test", function() {});
+                    innerResult2 = sut.test("Inner Test 2", function() {});
+                    innerResult3 = sut.test("Inner Test 3", function() {});
+                });
+
+                then(function() { expect(result.isComplete()).to.be.true; });
+                then(function() { expect(result.runCount()).to.equal(3); });
+                then(function() { expect(result.children()).to.deep.equal([innerResult, innerResult2, innerResult3]); });
+            });
+
+            when("running a test method with a double nested test method call", function() {
+                var innerResult, innerInnerResult, innerInnerResult2;
+                var result = sut.test("Outer Test", function() {
+                    innerResult = sut.test("Inner Test", function() {
+                        innerInnerResult = sut.test("Inner Inner Test", function() {});
+                        innerInnerResult2 = sut.test("Inner Inner Test 2", function() {});
+                    });
+                });
+
+                then(function() { expect(result.isComplete()).to.be.true; });
+                then(function() { expect(result.runCount()).to.equal(2); });
+                then(function() { expect(result.children()).to.deep.equal([innerResult]); });
+                then(function() { expect(innerResult.children()).to.deep.equal([innerInnerResult, innerInnerResult2]); });
+            });
+
+            when("running a named test method without specifying a name", function() {
+                var result = sut.test(function NamedFunctionName () {});
+                then(function() { expect(result.name()).to.equal("NamedFunctionName"); });
+            });
+
+            when("running an unnamed test method without specifying a name", function() {
+                var result = sut.test(function() { expect('name').to.equal('name'); });
+                then(function() { expect(result.name()).to.equal("expect name to equal name"); });
+            });
+
+            when("running a named test method without specifying a name", function() {
+                var result = sut.test(function TestName () {});
+
+                then("name extracted from test name", function() {
+                    expect(result.name()).to.equal("TestName");
+                });
+            });
+        });
+
+        describe("plugins", function() {
+            when("registering a root plugin", function() {
+                var pluginFunction = sinon.stub();
+                sut.registerRootPlugin(pluginFunction);
+
+                then(function() { expect(pluginFunction).to.not.have.been.called; });
+
+                when("plugin does not yield", function() {
+                    it("throws", function() {
+                        expect(function() { sut.test("TestName", function() {}); }).to.throw();
+                    });
+                });
+
+                when("plugin yields", function() {
+                    pluginFunction.yields();
+                    var result = sut.test("TestName", function() {});
+
+                    then(function() { expect(pluginFunction).to.have.been.called; });
+                    then(function() { expect(result.isComplete()).to.be.true; });
+                });
+
                 pluginFunction.yields();
+
+                when("registering a second root plugin", function() {
+                    var pluginFunction2 = sinon.stub().yields();
+                    sut.registerRootPlugin(pluginFunction2);
+
+                    when("running test", function() {
+                        sut.test("TestName", function() {});
+                        then(function() {expect(pluginFunction2).to.have.been.calledBefore(pluginFunction);});
+                    });
+                });
+            });
+
+            when("root plugin callback returns", function() {
+                var callbackReturnValue;
+                var pluginFunction = function(cb) {
+                    callbackReturnValue = cb();
+                };
+                sut.registerRootPlugin(pluginFunction);
                 var result = sut.test("TestName", function() {});
 
-                then(function() { expect(pluginFunction).to.have.been.called; });
-                then(function() { expect(result.isComplete()).to.be.true; });
+                then(function() { expect(callbackReturnValue).to.equal(result); });
             });
 
-            pluginFunction.yields();
+            when("register a setup plugin", function() {
+                var pluginFunction = sinon.stub();
+                sut.registerSetupPlugin(pluginFunction);
 
-            when("registering a second root plugin", function() {
-                var pluginFunction2 = sinon.stub().yields();
-                sut.registerRootPlugin(pluginFunction2);
+                then(function() { expect(pluginFunction).to.not.have.been.called; });
 
-                when("running test", function() {
-                    sut.test("TestName", function() {});
-                    then(function() {expect(pluginFunction2).to.have.been.calledBefore(pluginFunction);});
+                when("plugin does not yield", function() {
+                    it("throws", function() {
+                        expect(function() { sut.test("TestName", function() {}); }).to.throw();
+                    });
                 });
-            });
-        });
 
-        when("root plugin callback returns", function() {
-            var callbackReturnValue;
-            var pluginFunction = function(cb) {
-                callbackReturnValue = cb();
-            };
-            sut.registerRootPlugin(pluginFunction);
-            var result = sut.test("TestName", function() {});
+                when("running 2 nested tests", function() {
+                    pluginFunction.yields();
+                    var innerTest1 = sinon.stub();
+                    var innerTest2 = sinon.stub();
 
-            then(function() { expect(callbackReturnValue).to.equal(result); });
-        });
+                    sut.test(function OuterTest () {
+                        sut.test("InnerTest1", innerTest1);
+                        sut.test("InnerTest2", innerTest2);
+                    });
 
-        when("register a setup plugin", function() {
-            var pluginFunction = sinon.stub();
-            sut.registerSetupPlugin(pluginFunction);
-
-            then(function() { expect(pluginFunction).to.not.have.been.called; });
-
-            when("plugin does not yield", function() {
-                it("throws", function() {
-                    expect(function() { sut.test("TestName", function() {}); }).to.throw();
+                    then(function() { expect(pluginFunction).to.have.been.calledTwice;});
+                    then(function() { expect(innerTest1).to.have.been.calledOn(pluginFunction.firstCall.thisValue);});
+                    then(function() { expect(innerTest2).to.have.been.calledOn(pluginFunction.secondCall.thisValue);});
                 });
             });
 
-            when("running 2 nested tests", function() {
-                pluginFunction.yields();
-                var innerTest1 = sinon.stub();
-                var innerTest2 = sinon.stub();
+            when("setup plugin callback returns", function() {
+                var callbackReturnValue;
+                var pluginFunction = function(cb) {
+                    callbackReturnValue = cb();
+                };
+                sut.registerSetupPlugin(pluginFunction);
+                var result = sut.test("TestName", function() {});
 
-                sut.test(function OuterTest() {
-                    sut.test("InnerTest1", innerTest1);
-                    sut.test("InnerTest2", innerTest2);
-                });
-
-                then(function(){ expect(pluginFunction).to.have.been.calledTwice;});
-                then(function(){ expect(innerTest1).to.have.been.calledOn(pluginFunction.firstCall.thisValue);});
-                then(function(){ expect(innerTest2).to.have.been.calledOn(pluginFunction.secondCall.thisValue);});
+                then(function() { expect(callbackReturnValue).to.be.null; });
             });
-        });
-
-        when("setup plugin callback returns", function() {
-            var callbackReturnValue;
-            var pluginFunction = function(cb) {
-                callbackReturnValue = cb();
-            };
-            sut.registerSetupPlugin(pluginFunction);
-            var result = sut.test("TestName", function() {});
-
-            then(function() { expect(callbackReturnValue).to.be.null; });
         });
     });
 
